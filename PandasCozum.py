1b2002c1b28ab86ad2dba7139c14951ca7dec3fd
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-04-05 16:18:09 +0300

    PANEL DENEMESİ

--- PandasCozum.py
+++ PandasCozum.py
@@ -21,6 +21,17 @@ print(dfO)
 
 df1 = dfO.iloc[:,1:6].T
 print(df1)
+print(df1.iloc[:,0])
+dfDeneme = pd.DataFrame(df1.iloc[:,0])
+dfDeneme.columns = ["J8"]
+# print(dfDeneme.sort_values(by=0))
+data = { "J8":dfDeneme.sort_values(by="J8") 
+}
+# dfDeneme = pd.DataFrame(df1.iloc[:,1])
+# data.update({"J5":dfDeneme.sort_values(by=1)})
+
+p = pd.Panel(data)
+print(p["J8"])
 
 
 
