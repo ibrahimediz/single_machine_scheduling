d0c8aceec6effdfcda9c5683a01f2fb97bae2467
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-04-05 16:48:24 +0300

    orderlar sıralandı

--- PandasCozum.py
+++ PandasCozum.py
@@ -19,19 +19,34 @@ dfO = df.sort_values(by="SUM",ascending=1).reset_index(drop=True)
 
 print(dfO)
 
-df1 = dfO.iloc[:,1:6].T
-print(df1)
-print(df1.iloc[:,0])
-dfDeneme = pd.DataFrame(df1.iloc[:,0])
-dfDeneme.columns = ["J8"]
-# print(dfDeneme.sort_values(by=0))
-data = { "J8":dfDeneme.sort_values(by="J8") 
-}
-# dfDeneme = pd.DataFrame(df1.iloc[:,1])
-# data.update({"J5":dfDeneme.sort_values(by=1)})
 
-p = pd.Panel(data)
-print(p["J8"])
+
+
+df1 = dfO.iloc[:,1:6].T
+df1.columns=dfO.iloc[:,0]
+siralama = {}
+jobListe = dfO.iloc[:,0]
+for i in range(int(df1.shape[1])) :
+    dfDeneme = pd.DataFrame(df1.iloc[:,i])
+    dfDeneme = dfDeneme.sort_values(by=list(dfDeneme)[0])
+    siralama[list(dfDeneme)[0]] = dfDeneme.index.tolist()
+print(siralama)
+
+
+for item in jobListe:
+    print(siralama[item])
+
+# print(df1.iloc[:,0])
+# dfDeneme = pd.DataFrame(df1.iloc[:,0])
+# dfDeneme.columns = ["J8"]
+# # print(dfDeneme.sort_values(by=0))
+# data = { "J8":dfDeneme.sort_values(by="J8") 
+# }
+# # dfDeneme = pd.DataFrame(df1.iloc[:,1])
+# # data.update({"J5":dfDeneme.sort_values(by=1)})
+
+# p = pd.Panel(data)
+# print(p["J8"])
 
 
 
