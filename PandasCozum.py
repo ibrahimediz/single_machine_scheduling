a5a3d41e0c263cd97ea5dc4bfd6e9686f6235315
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-04-10 18:28:30 +0300

    sıfırlar alındı

--- PandasCozum.py
+++ PandasCozum.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 sutun = ["Job","O1","O2","O3","O4","O5","PROC","SETUP"]
 data =  pd.read_csv(r"E:\Projelerim\single_machine_scheduling\DENEME CSV SETLER\DENEME CSV SETLER\I=5, J=10\Experiment35.csv",delimiter=";")
-
+# print(data)
 sutun.append("SUM")
 df = pd.DataFrame(columns=sutun)
 df.Job = data.Job
@@ -12,25 +12,43 @@ df.iloc[:,1:6] = data.iloc[:,2:7]
 for i in range(1,11):
     dr =  (data.iloc[i-1,2:7].sum()*data.iloc[i-1,7])+data.iloc[i-1,8]
     df.SUM.iloc[[i-1]] = dr
-
+# print(df)
 for i in range(1,6):
     df["O"+str(i)]=df["O"+str(i)]*df["PROC"]
 dfO = df.sort_values(by="SUM",ascending=1).reset_index(drop=True)
 
-print(dfO)
+# print(dfO)
 
 
 
 
 df1 = dfO.iloc[:,1:6].T
+# print(df1)
 df1.columns=dfO.iloc[:,0]
 siralama = {}
 jobListe = dfO.iloc[:,0]
 for i in range(int(df1.shape[1])) :
     dfDeneme = pd.DataFrame(df1.iloc[:,i])
     dfDeneme = dfDeneme.sort_values(by=list(dfDeneme)[0])
-    # print(dfDeneme[list(dfDeneme)[0]] != 0)
-    siralama[list(dfDeneme)[0]] = dfDeneme.index.tolist()
+    liste = dfDeneme.index.tolist()
+    liste2 = []
+    for i in dfDeneme[list(dfDeneme)[0]]:
+        liste1 = list(dfDeneme[list(dfDeneme)[0]])
+        liste2 = list(dfDeneme[list(dfDeneme)[0]].index)
+        print("1",liste1)
+        print("2",liste2)
+        for item in liste1:
+            if item == 0:
+                liste2.pop(liste1.index(item))
+        print("son",liste2)
+    siralama[list(dfDeneme)[0]] = liste2    
+    
+
+        # if i == 0:
+        #     liste.remove(liste.index())
+    # for i in liste
+    # print(type(dfDeneme[list(dfDeneme)[0]] != 0))
+    # siralama[list(dfDeneme)[0]] = liste
 print(siralama)
 
 
@@ -51,6 +69,9 @@ for i in range(len(jobListe),0,-1):
 
 for item in jobListe:
     print(item,siralama[item])
+
+
+
         # if list(set(siralama[jobListe[i-1]]) & set(siralama[jobListe[i-2]]))[0] > 0:
         #     print(siralama[jobListe[i-1]])
     
