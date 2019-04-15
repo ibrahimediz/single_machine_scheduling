44f3471d40b441ae1d9dbd979bf34e8582fa0e47
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-04-15 16:20:53 +0300

    Toplam ve Order Çalışıldı

--- PandasCozum.py
+++ PandasCozum.py
@@ -24,13 +24,14 @@ for i in range(1,6):
 dfO = df.sort_values(by="SUM",ascending=1).reset_index(drop=True)
 
 
-print(dfO)
 
 
 df1 = dfO.iloc[:,1:6].T
 df1.columns=dfO.iloc[:,0]
 df2 = dfO.iloc[:,1:8].T
 df2.columns=dfO.iloc[:,0]
+
+print(df1)
 print(df2)
 siralama = {}
 jobListe = dfO.iloc[:,0]
@@ -48,6 +49,8 @@ for i in range(int(df1.shape[1])) :
     siralama[list(dfDeneme)[0]] = liste2    
     
 
+
+
         # if i == 0:
         #     liste.remove(liste.index())
     # for i in liste
@@ -58,26 +61,41 @@ for i in range(int(df1.shape[1])) :
 # for item in jobListe:
 #     print(item,siralama[item])
 
+def JobYukSirala(dfData,Liste,Job):
+    # print(dfData,Liste,Job)
+    listeOrderSure = []
+    for i in liste:
+        listeOrderSure.append((i,dfData.loc[str(i),str(Job)]))
+    labels = ["Order","Value"]
+    dataFrame = pd.DataFrame.from_records(listeOrderSure,columns=labels)
+    dataFrame = dataFrame.sort_values(by="Value",ascending=1)
+    # print(Job,list(dataFrame.loc[:,"Order"]))
+    return list(dataFrame.loc[:,"Order"])
+
+
 for i in range(len(jobListe),0,-1):
     if not i - 1 == 0:
         listeA = siralama[jobListe[i-1]]
         listeB = siralama[jobListe[i-2]]
         liste = []
-        for a in listeA:
-            for b in listeB:
+        for a in listeB:
+            for b in listeA:
                 if a==b:
                     liste.append(a)
         if len(liste)>0:
-            print(liste)        
+            print("1-",jobListe[i-1],liste)
+            liste = JobYukSirala(df2,listeA,jobListe[i-1])
+            print("2-",jobListe[i-1],liste)
+        if len(liste)>0:      
             for c in reversed(liste):
-                listeB.remove(c)
-                listeB.insert(0,c)   
-            siralama[jobListe[i-2]] = listeB
+                listeA.remove(c)
+                listeA.insert(0,c)
+            
+            siralama[jobListe[i-1]] = listeA
 
 
 # for item in jobListe:
 #     print(item,siralama[item])
-
 
 
 toplam = 0
@@ -85,36 +103,65 @@ orderToplamListesi = {}
 for order in range(1,6):
     orderToplamListesi.update({"O"+str(order):0})
 
-def ilerideVarmi(order,Job):
-    global jobListe
-    global siralama
-    liste = list(jobListe)
-    if liste.index(Job)+1  == len(liste):
-        return False
-    else:
-        sayi = liste.index(Job)
-        sinama  = False
-        for i in range(sayi,len(liste)):
-            for item in siralama[liste[i]] :
-                if order in item:
-                    sinama = True
-        return sinama
-                    
-# print(list(jobListe))
+def toplama(Order,Value):
+    global toplam
+    global orderToplamListesi
+    orderToplamListesi[Order]=Value
+    
+
+
+
 for item in jobListe:
     toplam += df2.loc["SETUP",item]
     for i in siralama[item]:
-        print(item,i,ilerideVarmi(i,item))
         toplam += df2.loc[i,item]
-        if ilerideVarmi(i,item):            
-            orderToplamListesi[i] = toplam
-    print("Toplam",toplam)
-print(orderToplamListesi)
+        toplama(i,toplam)    
+
 sayi = 0
 for i in orderToplamListesi.values():
     sayi += i
 
 print("Toplam",sayi)
+    
+
+
+# toplam = 0
+# orderToplamListesi = {}
+# for order in range(1,6):
+#     orderToplamListesi.update({"O"+str(order):0})
+
+# def ilerideVarmi(order,Job):
+#     global jobListe
+#     global siralama
+#     liste = list(jobListe)
+#     if liste.index(Job)+1  == len(liste):
+#         return False
+#     else:
+#         sayi = liste.index(Job)
+#         sinama  = False
+#         for i in range(sayi,len(liste)):
+#             for item in siralama[liste[i]] :
+#                 if order in item:
+#                     sinama = True
+#         return sinama
+
+
+
+# # print(list(jobListe))
+# for item in jobListe:
+#     toplam += df2.loc["SETUP",item]
+#     for i in siralama[item]:
+#         print(item,i,ilerideVarmi(i,item))
+#         toplam += df2.loc[i,item]
+#         if ilerideVarmi(i,item):            
+#             orderToplamListesi[i] = toplam
+#     print("Toplam",toplam)
+# print(orderToplamListesi)
+# sayi = 0
+# for i in orderToplamListesi.values():
+#     sayi += i
+
+# print("Toplam",sayi)
 
 
                     
