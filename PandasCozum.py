02508fac2fdb859714d4bcc33b1b42da90aa169e
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-04-25 10:40:53 +0300

    raporlar

--- PandasCozum.py
+++ PandasCozum.py
@@ -8,122 +8,166 @@
 
 import pandas as pd
 import numpy as np
-sutun = ["Job","O1","O2","O3","O4","O5","PROC","SETUP"]
-data =  pd.read_csv(r"E:\Projelerim\single_machine_scheduling\DENEME CSV SETLER\DENEME CSV SETLER\I=5, J=10\Experiment35.csv",delimiter=";")
-sutun.append("SUM")
-df = pd.DataFrame(columns=sutun)
-df.Job = data.Job
-df.SETUP = data.SETUP
-df.PROC=data.PROC
-df.iloc[:,1:6] = data.iloc[:,2:7]
-for i in range(1,11):
-    dr =  (data.iloc[i-1,2:7].sum()*data.iloc[i-1,7])+data.iloc[i-1,8]
-    df.SUM.iloc[[i-1]] = dr
-for i in range(1,6):
-    df["O"+str(i)]=df["O"+str(i)]*df["PROC"]
-dfO = df.sort_values(by="SUM",ascending=1).reset_index(drop=True)
+import os
+def DosyaOlustur(adres):
+    import os
+    try:
+        file=None
+        if not os.path.exists(adres):
+            file = open(adres,"w")
+        else :
+            file = open(adres,"r+")
+    except Exception as Hata:
+        file.write(str(Hata))
+    finally:
+        file.close()
 
 
 
-
-df1 = dfO.iloc[:,1:6].T
-df1.columns=dfO.iloc[:,0]
-df2 = dfO.iloc[:,1:8].T
-df2.columns=dfO.iloc[:,0]
-
-print(df1)
-print(df2)
-siralama = {}
-jobListe = dfO.iloc[:,0]
-for i in range(int(df1.shape[1])) :
-    dfDeneme = pd.DataFrame(df1.iloc[:,i])
-    dfDeneme = dfDeneme.sort_values(by=list(dfDeneme)[0])
-    liste = dfDeneme.index.tolist()
-    liste2 = []
-    for i in dfDeneme[list(dfDeneme)[0]]:
-        liste1 = list(dfDeneme[list(dfDeneme)[0]])
-        liste2 = list(dfDeneme[list(dfDeneme)[0]].index)
-        for item in liste1:
-            if item == 0:
-                liste2.pop(liste1.index(item))
-    siralama[list(dfDeneme)[0]] = liste2    
+def OkuBakalım(is_adi,Folderadres,txt_adres,csv_adres,Job,Order):
+    sutun = ["Job"] 
+    for i in range (1,Order+1):
+        sutun.append("O"+str(i))
+    sutun += ["PROC","SETUP"]
+    adres = Folderadres+"\\"+txt_adres
+    data =  pd.read_csv(Folderadres+"\\"+csv_adres,delimiter=";")
+    DosyaOlustur(Folderadres+"\\"+txt_adres)
+    print("İlk Okuma\n",is_adi,file = open(adres,"a"))
+    print(data,"\n",is_adi*2,file = open(adres,"a"))
     
+    sutun.append("SUM")
+    df = pd.DataFrame(columns=sutun)
+    df.Job = data.Job
+    df.SETUP = data.SETUP
+    df.PROC=data.PROC
+    df.iloc[:,1:Order+1] = data.iloc[:,2:Order+2]
+    for i in range(1,Job+1):
+        dr =  (data.iloc[i-1,2:Order+2].sum()*data.iloc[i-1,Order+2])+data.iloc[i-1,Order+3]
+        df.SUM.iloc[[i-1]] = dr
+    for i in range(1,Order+1):
+        df["O"+str(i)]=df["O"+str(i)]*df["PROC"]
+    dfO = df.sort_values(by="SUM",ascending=1).reset_index(drop=True)
+
+    print("Sum Eklendi\n",is_adi,file = open(adres,"a"))
+    print(dfO,"\n"*2,file = open(adres,"a"))
 
 
 
-        # if i == 0:
-        #     liste.remove(liste.index())
-    # for i in liste
-    # print(type(dfDeneme[list(dfDeneme)[0]] != 0))
-    # siralama[list(dfDeneme)[0]] = liste
+    df1 = dfO.iloc[:,1:Order+1].T
+    df1.columns=dfO.iloc[:,0]
+
+    print("df1 Eklendi\n",is_adi,file = open(adres,"a"))
+    print(df1,"\n"*2,file = open(adres,"a"))
 
 
-# for item in jobListe:
-#     print(item,siralama[item])
+    df2 = dfO.iloc[:,1:Order+3].T
+    df2.columns=dfO.iloc[:,0]
 
-def JobYukSirala(dfData,Liste,Job):
-    # print(dfData,Liste,Job)
-    listeOrderSure = []
-    for i in liste:
-        listeOrderSure.append((i,dfData.loc[str(i),str(Job)]))
-    labels = ["Order","Value"]
-    dataFrame = pd.DataFrame.from_records(listeOrderSure,columns=labels)
-    dataFrame = dataFrame.sort_values(by="Value",ascending=1)
-    # print(Job,list(dataFrame.loc[:,"Order"]))
-    return list(dataFrame.loc[:,"Order"])
+    print("df2 Eklendi\n",is_adi,file = open(adres,"a"))
+    print(df2,"\n"*2,file = open(adres,"a"))
 
 
-for i in range(len(jobListe),0,-1):
-    if not i - 1 == 0:
-        listeA = siralama[jobListe[i-1]]
-        listeB = siralama[jobListe[i-2]]
-        liste = []
-        for a in listeB:
-            for b in listeA:
-                if a==b:
-                    liste.append(a)
-        if len(liste)>0:
-            print("1-",jobListe[i-1],liste)
-            liste = JobYukSirala(df2,listeA,jobListe[i-1])
-            print("2-",jobListe[i-1],liste)
-        if len(liste)>0:      
-            for c in reversed(liste):
-                listeA.remove(c)
-                listeA.insert(0,c)
+
+    siralama = {}
+    jobListe = dfO.iloc[:,0]
+    for i in range(int(df1.shape[1])) :
+        dfDeneme = pd.DataFrame(df1.iloc[:,i])
+        dfDeneme = dfDeneme.sort_values(by=list(dfDeneme)[0])
+        liste = dfDeneme.index.tolist()
+        liste2 = []
+        for i in dfDeneme[list(dfDeneme)[0]]:
+            liste1 = list(dfDeneme[list(dfDeneme)[0]])
+            liste2 = list(dfDeneme[list(dfDeneme)[0]].index)
+            for item in liste1:
+                if item == 0:
+                    liste2.pop(liste1.index(item))
+        siralama[list(dfDeneme)[0]] = liste2    
+
+
+    print("JobListe Eklendi\n",is_adi,file = open(adres,"a"))
+    print(jobListe,"\n"*2,file = open(adres,"a"))
+
+
+    def JobYukSirala(dfData,Liste,Job):
+        # print(dfData,Liste,Job)
+        listeOrderSure = []
+        for i in liste:
+            listeOrderSure.append((i,dfData.loc[str(i),str(Job)]))
+        labels = ["Order","Value"]
+        dataFrame = pd.DataFrame.from_records(listeOrderSure,columns=labels)
+        dataFrame = dataFrame.sort_values(by="Value",ascending=1)
+        # print(Job,list(dataFrame.loc[:,"Order"]))
+        return list(dataFrame.loc[:,"Order"])
+
+
+    for i in range(len(jobListe),0,-1):
+        if not i - 1 == 0:
+            listeA = siralama[jobListe[i-1]]
+            listeB = siralama[jobListe[i-2]]
+            liste = []
+            for a in listeB:
+                for b in listeA:
+                    if a==b:
+                        liste.append(a)
+            if len(liste)>0:
+                liste = JobYukSirala(df2,listeA,jobListe[i-1])
+                print("Fark Listesi-",jobListe[i-1],liste)
+            if len(liste)>0:      
+                for c in reversed(liste):
+                    listeA.remove(c)
+                    listeA.insert(0,c)
+                
+                siralama[jobListe[i-1]] = listeA
+
+    print("Sıralama Eklendi\n",is_adi,file = open(adres,"a"))
+    print(siralama,"\n"*2,file = open(adres,"a"))
+
+
+
+
+    toplam = 0
+    orderToplamListesi = {}
+    for order in range(1,Order+1):
+        orderToplamListesi.update({"O"+str(order):0})
+
+    def toplama(Order,Value):
+        orderToplamListesi[Order]=Value
+        
+
+
+
+    for item in jobListe:
+        toplam += df2.loc["SETUP",item]
+        for i in siralama[item]:
+            toplam += df2.loc[i,item]
+            toplama(i,toplam)
+
+
+    print("orderToplamListesi Eklendi\n",is_adi,file = open(adres,"a"))
+    print(orderToplamListesi,"\n"*2,file = open(adres,"a"))
+
             
-            siralama[jobListe[i-1]] = listeA
+
+    sayi = 0
+    for i in orderToplamListesi.values():
+        sayi += i
 
 
-# for item in jobListe:
-#     print(item,siralama[item])
+    print("Toplam Eklendi\n",is_adi,file = open(adres,"a"))
+    print(sayi,"\n"*2,file = open(adres,"a"))
 
-
-toplam = 0
-orderToplamListesi = {}
-for order in range(1,6):
-    orderToplamListesi.update({"O"+str(order):0})
-
-def toplama(Order,Value):
-    global toplam
-    global orderToplamListesi
-    orderToplamListesi[Order]=Value
-    
-
-
-
-for item in jobListe:
-    toplam += df2.loc["SETUP",item]
-    for i in siralama[item]:
-        toplam += df2.loc[i,item]
-        toplama(i,toplam)    
-
-sayi = 0
-for i in orderToplamListesi.values():
-    sayi += i
-
-print("Toplam",sayi)
-    
-
+    print("Toplam",sayi)
+        
+table_list = []
+new_table_list = []
+for filename in os.listdir(r"E:\Projelerim\single_machine_scheduling\DENEME CSV SETLER\DENEME CSV SETLER\I=15, J=20"):
+    if filename.endswith('.csv'):
+        # table_list.append(pd.read_csv(filename,sep="|"))
+        new_table_list.append(filename)
+        # print(table_list)
+        # print(new_table_list)
+for item in new_table_list:
+    OkuBakalım(item.split(".")[0],r"E:\Projelerim\single_machine_scheduling\DENEME CSV SETLER\DENEME CSV SETLER\I=15, J=20",r"\I15_J20.txt",item,Job=20,Order=15)
 
 # toplam = 0
 # orderToplamListesi = {}
